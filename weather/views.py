27c8e2d5a84118b77c044b5584632086ad7283f7
Tarakant <tarakant@debian>
2019-03-22 20:00:27 +0530

    complet weatherapp styles

--- weather/views.py
+++ weather/views.py
@@ -2,9 +2,22 @@ import requests
 from django.shortcuts import render
 # Create your views here.
 def index(request):
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid='
-    city = 'las vegas'
-    r = requests.get(url.format(city))
-    print(r.text)
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='
+    city = 'hyderabad'
+    req = requests.get(url.format(city)).json()
+    city_weather = {
 
-    return render(request,'weather/weather.html')
+        'city':req['name'],
+        'country':req['sys']['country'],
+        'actual_temp':req['main']['temp'],
+        'max_temp':req['main']['temp_max'],
+        'min_temp':req['main']['temp_min'],
+        'humidity':req['main']['humidity'],
+        'pressure':req['main']['pressure'],
+        'wind_speed':req['wind']['speed'],
+        'description':req['weather'][0]['description'],
+        'icon':req['weather'][0]['icon'],
+    }
+
+
+    return render(request,'weather/weather.html',{'city_weather':city_weather})
