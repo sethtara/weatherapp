b9d2d742ff0b3f8c94a73b4bcfa81e72ff41eb4f
Tarakant <tarakant@debian>
2019-03-23 00:22:25 +0530

    pin city in database from app

--- weather/views.py
+++ weather/views.py
@@ -1,23 +1,36 @@
 import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
 # Create your views here.
 def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='
-    city = 'hyderabad'
-    req = requests.get(url.format(city)).json()
-    city_weather = {
 
-        'city':req['name'],
-        'country':req['sys']['country'],
-        'actual_temp':req['main']['temp'],
-        'max_temp':req['main']['temp_max'],
-        'min_temp':req['main']['temp_min'],
-        'humidity':req['main']['humidity'],
-        'pressure':req['main']['pressure'],
-        'wind_speed':req['wind']['speed'],
-        'description':req['weather'][0]['description'],
-        'icon':req['weather'][0]['icon'],
-    }
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
 
+    form = CityForm()
 
-    return render(request,'weather/weather.html',{'city_weather':city_weather})
+    cities = City.objects.all()
+    weather_data=[]
+
+    for city in cities:
+
+        req = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city':req['name'],
+            'country':req['sys']['country'],
+            'actual_temp':req['main']['temp'],
+            'max_temp':req['main']['temp_max'],
+            'min_temp':req['main']['temp_min'],
+            'humidity':req['main']['humidity'],
+            'pressure':req['main']['pressure'],
+            'wind_speed':req['wind']['speed'],
+            'description':req['weather'][0]['description'],
+            'icon':req['weather'][0]['icon'],
+        }
+        weather_data.append(city_weather)
+
+    return render(request,'weather/weather.html',{'weather_data':weather_data,'form':form})
